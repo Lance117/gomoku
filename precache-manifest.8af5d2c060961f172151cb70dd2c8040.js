7204ec64a6ceb53a2a83ac67153cd4ee654d7e47
Lance Wong <lwong180@gmail.com>
2020-03-21 22:42:15 -0700

    Updates

---
+++ precache-manifest.8af5d2c060961f172151cb70dd2c8040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3be246e3efab6c1e08c195cc7b8c75d",
+    "url": "/gomoku/index.html"
+  },
+  {
+    "revision": "bf893d24eaa6c9cff996",
+    "url": "/gomoku/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5d2fdba8f247cf6b4674",
+    "url": "/gomoku/static/js/2.4dbdb96f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gomoku/static/js/2.4dbdb96f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf893d24eaa6c9cff996",
+    "url": "/gomoku/static/js/main.694af235.chunk.js"
+  },
+  {
+    "revision": "cd138d1ccd6f0321aaf2",
+    "url": "/gomoku/static/js/runtime-main.2e4dc78a.js"
+  }
+]);
