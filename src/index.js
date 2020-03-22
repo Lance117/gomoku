3bc13534633681f17a42e673177711e5e051cbd3
Lance Wong <lwong180@gmail.com>
2020-03-21 23:40:14 -0700

    gomoku game, checks rows and cols for winners. todo: check diagonals

--- src/index.js
+++ src/index.js
@@ -1,17 +1,127 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Square(props) {
+    return (
+        <button className="square" onClick={() => props.onClick()}>
+            {props.value}
+        </button>
+    );
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    renderBoard() {
+        const board = [];
+        for (let i = 0; i < 15; i++) {
+            let row = [];
+            for (let j = i * 15; j < i * 15 + 15; j++) {
+                row.push(this.renderSquare(j));
+            }
+            board.push(<div className="board-row">{row}</div>);
+        }
+        return board;
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                {this.renderBoard()}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function calculateLines() {
+    const rows = [];
+    const cols = [];
+    for (let i = 0; i < 15; i++) {
+        // get winning row lines
+        for (let j = i * 15; j <= i * 15 + 10; j++) {
+            rows.push([j, j+1, j+2, j+3, j+4]);
+        }
+        // get winning col lines
+        for (let j = i; j <= i + 150; j += 15) {
+            cols.push([j, j+15, j+30, j+45, j+60]);
+        }
+    }
+    return rows.concat(cols);
+}
+
+function calculateWinner(squares) {
+    const lines = calculateLines();
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c, d, e] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c] &&
+            squares[a] === squares[d] &&
+            squares[a] === squares[e]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
+}
